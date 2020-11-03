31ca738fffde3f54edf758c82dc1afa64999abf2
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-11-03 20:57:32 +0100

    Add tests

--- src/main/java/org/jobrunr/examples/api/JobController.java
+++ src/main/java/org/jobrunr/examples/api/JobController.java
@@ -2,11 +2,14 @@ package org.jobrunr.examples.api;
 
 import org.jobrunr.examples.services.SampleJobService;
 import org.jobrunr.scheduling.JobScheduler;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 import static java.time.temporal.ChronoUnit.HOURS;
 
@@ -28,8 +31,10 @@ public class JobController {
     }
 
     @GetMapping("/schedule-example-job")
-    public String scheduleExampleJob(@RequestParam(value = "name", defaultValue = "World") String name) {
-        jobScheduler.schedule(() -> sampleService.executeSampleJob("Hello " + name), Instant.now().plus(3, HOURS));
+    public String scheduleExampleJob(
+            @RequestParam(value = "name", defaultValue = "World") String name,
+            @RequestParam(value = "when", defaultValue = "PT3H") String when) {
+        jobScheduler.schedule(() -> sampleService.executeSampleJob("Hello " + name), Instant.now().plus(Duration.parse(when)));
         return "Job Enqueued";
     }
 }
